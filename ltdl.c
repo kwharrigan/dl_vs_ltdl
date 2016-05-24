dc55ff3a631d20c091947fcfc3fed1976d53da1e
Kyle Harrigan <kwharrigan@gmail.com>
2016-05-23 23:28:28 -0400

    Remove junk lines

--- ltdl.c
+++ ltdl.c
@@ -11,16 +11,7 @@ int main(int argc, char **argv) {
 
     lt_dlinit();
     handle = lt_dlopen ("/lib64/libm.so.6");
-    //if (!handle) {
-    //    fputs (dlerror(), stderr);
-    //    exit(1);
-    //}
     cosine = lt_dlsym(handle, "cos");
-    //if ((error = dlerror()) != NULL)  {
-    //    fputs(error, stderr);
-    //    exit(1);
-    //}
-
     printf ("%f\n", (*cosine)(2.0));
     lt_dlclose(handle);
 }
